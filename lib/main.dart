505da56b382560e47dd165f447f2e2761b560226
Emeka <emekannecam@gmail.com>
2020-09-13 03:40:06 -0500

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue[900],
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text('Random Dice Generator'),
+          backgroundColor: Colors.blue,
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,70 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void diceNumber() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+      leftDiceNumber = Random().nextInt(6) + 1;
+      textChange = 'Dice face changed.';
+    });
+  }
+
+  String textChange = 'Click the dices';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  '$textChange',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: FlatButton(
+                    onPressed: () {
+                      //Change the leftDiceNumber and rightDiceNumber state onPressed method.
+                      diceNumber();
+                    },
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: FlatButton(
+                    onPressed: () {
+                      diceNumber(); //Change the leftDiceNumber and rightDiceNumber state onPressed method.
+                    },
+                    child: Image.asset('images/dice$rightDiceNumber.png'),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
